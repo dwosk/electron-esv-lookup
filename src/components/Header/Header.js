b5843e167ff2f3f0e898e01343056aa459e86088
David Wosk <davidwosk1@gmail.com>
2019-09-06 23:40:58 -0700

    fix: add version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,6 +18,7 @@ import ToggleItem from './ToggleItem';
 import NotificationAction from './NotificationAction';
 import Settings20 from '@carbon/icons-react/lib/settings/20';
 import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import { version } from '../../../package.json';
 
 const LookupHeader = () => {
   const [rightPanel, toggleRightPanel] = useState(
@@ -74,10 +75,10 @@ const LookupHeader = () => {
           <SwitcherDivider />
         </Switcher>
       </HeaderPanel>
-      <Modal open={about} passiveModal modalHeading="About" modalLabel="ESV Lookup" onRequestClose={() => {toggleAbout(false)}}>
+      <Modal open={about} passiveModal modalHeading="ESV Lookup" modalLabel="About" onRequestClose={() => {toggleAbout(false)}}>
+        <h4>Version {version}</h4>
         <h4>Author - David Wosk</h4>
         <h4>Copyright - All Rights Reserved</h4>
-
         <article style={{paddingTop: "1em"}}>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. You may not copy or download more than 500 consecutive verses of the ESV Bible or more than one half of any book of the ESV Bible.</article>
       </Modal>
     </Header>
